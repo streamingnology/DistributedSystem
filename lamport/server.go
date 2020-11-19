e4445f8d32852bc6687b399b0159d7837adc988a
streamingnology <37998919+streamingnology@users.noreply.github.com>
2020-11-19 15:27:35 -0600

    Update server.go
    more log

--- lamport/server.go
+++ lamport/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -13,37 +14,43 @@ import (
 var serverMutex sync.Mutex
 var usingSharedResource bool = false
 var sharedResourceUser  string
+var recordAccess map[string]int
 
-type SharedResourceService struct {
-
-}
+type SharedResourceService struct { }
 
 func (*SharedResourceService) RequestSharedResource(req string, res *string) error {
-	fmt.Println(req, " is requesting shared resource")
 	serverMutex.Lock()
+	defer serverMutex.Unlock()
+	fmt.Println(req, " is requesting shared resource")
+
+	recordAccess[req] = recordAccess[req]+1
+	for i:=0; i < 5; i++ {
+		fmt.Printf("      %d : %d\n", i, recordAccess[fmt.Sprintf("%d",i)])
+	}
+
 	if usingSharedResource {
+		s := sharedResourceUser + "is using shared resource"
 		fmt.Println(sharedResourceUser, " is using shared resource")
 		fmt.Println("Fatal Error occurred when request resource, Please check your Lamport Algorithm!!!!")
-		return nil
+		return errors.New(s)
 	}
 	sharedResourceUser = req
 	usingSharedResource = true
 	fmt.Println(req, " is using shared resource")
-	serverMutex.Unlock()
 	return nil
 }
 
 func (*SharedResourceService) ReleaseSharedResource(req string, res *string) error {
-	fmt.Println(req, " is releasing shared resource")
 	serverMutex.Lock()
+	defer serverMutex.Unlock()
+	fmt.Println(req, " is releasing shared resource")
 	if !usingSharedResource {
 		fmt.Println("Fatal Error occurred when releasing resource, Please check your Lamport Algorithm!!!!")
-		return nil
+		return errors.New("no user is using this shared resource")
 	}
 	sharedResourceUser = ""
 	usingSharedResource = false
 	fmt.Println(req, " released shared resource")
-	serverMutex.Unlock()
 	return nil
 }
 
@@ -58,6 +65,10 @@ func main() {
 	}
 	fmt.Printf("server address is %s:%s \n", ip, *p)
 
+	recordAccess = make(map[string]int)
+	for i := 0; i < 5; i++ {
+		recordAccess[fmt.Sprintf("%d",i)] = 0
+	}
 	rpc.Register(new(SharedResourceService))
 	rpc.HandleHTTP()
 	l, e := net.Listen("tcp", ":" + (*p))
